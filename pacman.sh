4121ed9eb4db79e654889a2d79bb732182742a8a
Vladimir <86943122+Izebergozz@users.noreply.github.com>
2022-11-13 20:58:32 +0700

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sudo pacman -Syu \
 sudo pacman-key --refresh-keys \
-sudo pacman -S artix-archlinux-support
+sudo pacman -S artix-archlinux-support \ 
+sudo pacman-key --populate archlinux
